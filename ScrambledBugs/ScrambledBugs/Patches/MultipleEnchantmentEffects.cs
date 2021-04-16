833897c835183b4967b901b46c74cfc85b645ccc
KernalsEgg <52105649+KernalsEgg@users.noreply.github.com>
2021-04-17 04:14:32 +0930

    2021-04-17 ScrambledBugs

--- ScrambledBugs/ScrambledBugs/Patches/MultipleEnchantmentEffects.cs
+++ ScrambledBugs/ScrambledBugs/Patches/MultipleEnchantmentEffects.cs
@@ -6,10 +6,14 @@
 		{
 			static Offsets()
 			{
+				Offsets.ImproveEffect = NetScriptFramework.Main.GameInfo.GetAddressOf(50366);
 				Offsets.IsNotCostliestEffect = NetScriptFramework.Main.GameInfo.GetAddressOf(50366, 0x23, 0, "0F85 C7010000"); // 6
 			}
 
 
+
+			/// <summary> SkyrimSE.exe + 0x868A00 </summary>
+			static internal System.IntPtr ImproveEffect { get; }
 
 			/// <summary> SkyrimSE.exe + 0x868A00 </summary>
 			static internal System.IntPtr IsNotCostliestEffect { get; }
@@ -20,6 +24,27 @@
 		internal MultipleEnchantmentEffects()
 		{
 			NetScriptFramework.Memory.WriteNop(MultipleEnchantmentEffects.Offsets.IsNotCostliestEffect, 6);
+
+			NetScriptFramework.Memory.WriteHook(new NetScriptFramework.HookParameters()
+			{
+				Address = MultipleEnchantmentEffects.Offsets.ImproveEffect + 0x126,
+				Pattern = "48 85 C0" + "74 4E",
+				ReplaceLength = 3 + 2,
+				IncludeLength = 3 + 2,
+				Before = cpuRegisters =>
+				{
+					var effect = cpuRegisters.BX;
+					var costliestEffect = NetScriptFramework.Memory.ReadPointer(cpuRegisters.DI + 0x28);
+
+					if (effect != costliestEffect)
+					{
+						cpuRegisters.XMM1f = cpuRegisters.XMM2f; // power = maximumPower
+						cpuRegisters.IP += 0x4E;
+
+						cpuRegisters.Skip();
+					}
+				},
+			});
 		}
 	}
 }
